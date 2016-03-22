1e6fdad243378dd50bf6d278859f3c3e6470e908
Craig Russell <craig.russell@synety.com>
2016-03-22 12:19:41 +0000

    Added 404 and 500 handlers. API routes

--- app.js
+++ app.js
@@ -10,24 +10,33 @@ var app = express();
 HOST = args.host || '127.0.0.1';
 PORT = args.port || 3000;
 
-// Parse body data as JSON
-// Requests must be sent with Content-Type:application/json
-app.use(parser.json());
-
-// Log request/responses
-app.use(log.middleware);
-
+app.use(parser.json());  // Parse request data as JSON
+app.use(log.middleware); // Log request/responses
 
 // Routes
 
-app.get('/', (req,res) => {
-    res.status(200).jsonp({msg: 'Hello World!'});
+app.get('/api/', (req, res) => {
+    res.jsonp({msg: 'Hello World!'});
 });
 
-app.post('/', (req,res) => {
-    res.status(200).jsonp({msg: req.body});
+app.post('/api/', (req, res) => {
+    res.jsonp({msg: req.body});
 });
 
+app.get('/api/500', (req, res) => {
+    throw Error(req.query.message || "Error!");
+});
+
+// 404 Handler
+app.use((req, res, next) => {
+    res.status(404).jsonp({error: "Not Found"})
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+    log.error(err);
+    res.status(500).jsonp({error: err.message});
+});
 
 // Listen on host and port
 app.listen(PORT, HOST, () => {
