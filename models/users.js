478122b5df0ac59d2f5f99ce2112c587cb73fa35
Craig Russell <craig.russell@synety.com>
2016-03-24 16:46:57 +0000

    Encrypt user passwords in DB

--- models/users.js
+++ models/users.js
@@ -3,8 +3,23 @@ var DB = require('../utils/sequelize');
 User = DB.sequelize.define('users', {
     id:       {type: DB.Sequelize.UUID,   primaryKey: true, defaultValue: DB.Sequelize.UUIDV4},
     username: {type: DB.Sequelize.STRING, allowNull: false, unique: true},
-    password: {type: DB.Sequelize.STRING, allowNull: false},
+    password: {type: DB.Sequelize.STRING, allowNull: false, set: setPassword},
     name:     DB.Sequelize.STRING,
 });
+
+/**
+ * Encrypt a string
+ */
+function encrypt(plaintext){
+    // TODO Not this.
+    return plaintext+Math.random();
+}
+
+/**
+ * Encrypt and store the password
+ */
+function setPassword(password){
+    return this.setDataValue('password', encrypt(password));
+}
 
 module.exports = User;
