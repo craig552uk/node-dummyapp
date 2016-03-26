350efebda38b983937d279b05f91a7ac5c9a7300
Craig Russell <craig@craig-russell.co.uk>
2016-03-26 18:54:01 +0000

    Encrypt user password

--- models/users.js
+++ models/users.js
@@ -1,25 +1,29 @@
-var DB = require('../utils/sequelize');
+var bcrypt = require('bcrypt');
+var DB     = require('../utils/sequelize');
 
 User = DB.sequelize.define('users', {
     id:       {type: DB.Sequelize.UUID,   primaryKey: true, defaultValue: DB.Sequelize.UUIDV4},
     username: {type: DB.Sequelize.STRING, allowNull: false, unique: true},
     password: {type: DB.Sequelize.STRING, allowNull: false, set: setPassword},
     name:     DB.Sequelize.STRING,
+}, {
+    instanceMethods: {checkPassword: checkPassword}
 });
-
-/**
- * Encrypt a string
- */
-function encrypt(plaintext){
-    // TODO Not this.
-    return plaintext+Math.random();
-}
 
 /**
  * Encrypt and store the password
  */
 function setPassword(password){
-    return this.setDataValue('password', encrypt(password));
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+    return this.setDataValue('password', hash);
+}
+
+/**
+ * Validate encrypted password
+ */
+function checkPassword(password){
+    return bcrypt.compareSync(password, this.password);
 }
 
 module.exports = User;
